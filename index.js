522ba3863be17ade4450947326e434dc347f1e94
Matthew English <matt.english4@gmail.com>
2024-11-22 10:33:59 -0330

    Add missing bcrypt import

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
+const bcrypt = require('bcrypt');
 
 const app = express();
 const PORT = 3000;
